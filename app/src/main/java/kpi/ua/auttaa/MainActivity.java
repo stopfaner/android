b8ce764bd7877a03dc13e62baff5411c2f8f8df2
OlexandrKovalchuk <me.olexandr.kovalchuk@gmail.com>
2014-08-30 21:05:35 +0300

    Added FloatingButtonLibrary

--- app/src/main/java/kpi/ua/auttaa/MainActivity.java
+++ app/src/main/java/kpi/ua/auttaa/MainActivity.java
@@ -25,6 +25,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //
+
         setContentView(R.layout.activity_main);
 
         googleMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map_fragment)).getMap();
