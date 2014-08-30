7259cc7eaa7a9c274820a09d9a59f209d4217fc7
OlexandrKovalchuk <me.olexandr.kovalchuk@gmail.com>
2014-08-30 21:34:53 +0300

    Added FAB

--- app/src/main/java/kpi/ua/auttaa/MainActivity.java
+++ app/src/main/java/kpi/ua/auttaa/MainActivity.java
@@ -8,8 +8,10 @@ import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.faizmalkani.floatingactionbutton.FloatingActionButton;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -30,6 +32,18 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
+        // There is floating actionButton initialization
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.start_alarm);
+        floatingActionButton.setColor(getResources().getColor(R.color.pink_900));
+        floatingActionButton.setDrawable(getResources().getDrawable(android.R.drawable.ic_menu_add));
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "LOLOLOLO", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         googleMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map_fragment)).getMap();
         googleMap.getUiSettings().setZoomControlsEnabled(false);
         googleMap.setMyLocationEnabled(true);
@@ -42,6 +56,8 @@ public class MainActivity extends Activity {
                 moveToLocation();
             }
         }, 1000); //delay to wait while map is loading
+
+
     }
 
     @Override
@@ -75,12 +91,12 @@ public class MainActivity extends Activity {
         if (myLocation == null) {
             Toast.makeText(this, R.string.location_invalid, Toast.LENGTH_SHORT).show();
             return;
-        } else {
-            double lattitude = myLocation.getLatitude();
-            double longitude = myLocation.getLongitude();
-
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lattitude, longitude), 13.5f));
         }
+
+        double lattitude = myLocation.getLatitude();
+        double longitude = myLocation.getLongitude();
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lattitude, longitude), 13.5f));
     }
 }
 
