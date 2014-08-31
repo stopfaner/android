79bc2c53af7d10674d28dbcb5217b5b2adb6a624
Dorosh Artyom <artem.dn0.o@gmail.com>
2014-08-31 05:41:32 +0300

    updated widget

--- app/src/main/java/kpi/ua/auttaa/widget/AlertWidget.java
+++ app/src/main/java/kpi/ua/auttaa/widget/AlertWidget.java
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import kpi.ua.auttaa.AlarmActivity;
 import kpi.ua.auttaa.R;
@@ -16,38 +17,21 @@ import kpi.ua.auttaa.R;
 public class AlertWidget extends AppWidgetProvider {
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        final int N = appWidgetIds.length;
-        for (int i=0; i<N; i++) {
-            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        for(int i=0; i<appWidgetIds.length; i++){
+            int currentWidgetId = appWidgetIds[i];
+
+            Intent intent = new Intent(context, AlarmActivity.class);
+
+            PendingIntent pending = PendingIntent.getActivity(context, 0,
+                    intent, 0);
+            RemoteViews views = new RemoteViews(context.getPackageName(),
+                    R.layout.alert_widget);
+            views.setOnClickPendingIntent(R.id.widget_start, pending);
+            appWidgetManager.updateAppWidget(currentWidgetId,views);
+            Toast.makeText(context, "widget added", Toast.LENGTH_SHORT).show();
         }
-    }
-
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-            int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.alert_widget);
-
-        Intent intent = new Intent(context, AlarmActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setOnClickPendingIntent(R.id.widget_start, pendingIntent);
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 }
 
